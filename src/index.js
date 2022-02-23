b9a5fdd8cf139c2419cbd0af45785b478ed150f8
yakusevich-silviya <silviya.yakusevich@gmail.com>
2022-02-24 01:23:41 +0200

    add functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+// function towelSort (matrix) {
 
-// You should implement your task here.
+const { underline } = require("colors");
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+// }
+
+module.exports = function towelSort(matrix) {
+    let result = [];
+
+    if (matrix) {
+        for (i = 0; i < matrix.length; i++) {
+            if (i !== 0 && i % 2 !== 0) {
+                matrix[i].reverse();
+            }
+        }
+
+        result = matrix.flat();
+    } else {
+        result = [];
+    }
+
+    return result;
+};
